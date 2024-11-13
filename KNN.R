7b2d747047f95a89243d0a796174e5aa8532f83b
Fatimakhan0 <124853832+Fatimakhan0@users.noreply.github.com>
2024-11-13 12:59:56 +0530

    Add files via upload

---
+++ KNN.R
@@ -0,0 +1,39 @@
+#KNN
+#Classification algorithm
+
+datam = read.csv("D:/7086/PimaIndians.csv")
+
+#Loading package 'class' for KNN
+library('class')
+#Loading package 'caret' for confusion matrix for validation
+install.packages('caret')
+library('caret')
+
+#Know the dataset
+View(datam)
+head(datam)
+#class() to know the datatype of the feature mass
+class(datam$BMI)
+#To know the Structure of data
+str(datam)
+#Convert class to factor
+datam[,'Outcome']=factor(datam[,'Outcome'])
+#Check the changes made to 'class' feature
+str(datam)
+
+#Finding the mean age of patients
+mean(datam$Age)
+#Giving info about mean,median and other descriptive values
+summary(datam)
+#building training data to train knn
+train=datam[1:500,]
+#building testing data to test knn
+test=datam[501:768,]
+#After training on the data
+pred_test=knn(train[,-9],test[,-9],train$Outcome,k=2)
+pred_test
+#table(gives us the correct and incorrect tests
+confusion=table(pred_test,test$Outcome)
+#Accuracy of model
+sum(diag(confusion))/nrow(test)
+confusionMatrix(pred_test,test$Outcome)
